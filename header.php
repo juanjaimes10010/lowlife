61511476b172b9e144c3647e7cff7318f43bf9de
juanjaimes10010 <67848711+juanjaimes10010@users.noreply.github.com>
2023-03-28 10:10:18 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+    <header>
+      <div class="container">
+        <nav>
+          <h1 id="logo">Dennis B&K</h1>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
